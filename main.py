9a3d43f2dd389f5293db9474d27c9024a1d3c90f
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-05-19 16:08:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from SeedSelection import *
 
 if __name__ == '__main__':
-    dataset_seq = [1, 2, 3, 4, 5]
+    dataset_seq = [1, 2, 3, 4]
     prod_seq = [1, 2]
     cm_seq = [1, 2]
     wd_seq = [1, 2]
     bi_seq = [1, 2]
 
     for data_setting in dataset_seq:
-        dataset_name = 'email' * (data_setting == 1) + 'dnc_email' * (data_setting == 2) + 'email_Eu_core' * (data_setting == 3) + \
-                       'WikiVote' * (data_setting == 4) + 'NetHEPT' * (data_setting == 5)
+        dataset_name = 'email' * (data_setting == 1) + 'dnc_email' * (data_setting == 2) + \
+                       'email_Eu_core' * (data_setting == 3) + 'NetHEPT' * (data_setting == 4)
         for cm in cm_seq:
             cascade_model = 'ic' * (cm == 1) + 'wc' * (cm == 2)
             for bi in bi_seq:
