b167a42604140c2adfb97769025da6c40452e739
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-05-20 15:44:43 +0800

    Update generateResultAnalysisFile.py

--- generateResultAnalysisFile.py
+++ generateResultAnalysisFile.py
@@ -1,6 +1,6 @@
 bi_seq = [1, 2]
 cm_seq = [1, 2]
-dataset_seq = [1, 2, 3, 4, 5]
+dataset_seq = [1, 2, 3, 4]
 prod_seq = [1, 2]
 wallet_distribution_seq = [1, 2]
 ppp_seq = [2, 3]
@@ -15,8 +15,8 @@ for bi in bi_seq:
     for cm in cm_seq:
         cascade_model = 'ic' * (cm == 1) + 'wc' * (cm == 2)
         for data_setting in dataset_seq:
-            dataset_name = 'email' * (data_setting == 1) + 'dnc_email' * (data_setting == 2) + 'email_Eu_core' * (data_setting == 3) + \
-                           'WikiVote' * (data_setting == 4) + 'NetHEPT' * (data_setting == 5)
+            dataset_name = 'email' * (data_setting == 1) + 'dnc_email' * (data_setting == 2) + \
+                           'email_Eu_core' * (data_setting == 3) + 'NetHEPT' * (data_setting == 4)
             for prod_setting in prod_seq:
                 product_name = 'item_lphc' * (prod_setting == 1) + 'item_hplc' * (prod_setting == 2)
                 for wallet_distribution in wallet_distribution_seq:
